7ced6b674d42118be02ade6bece75a51e394a8cd
estebancardona1 <esteban.cardona6@udea.edu.co>
2024-10-05 00:02:33 -0500

    Modelos1

--- c_modelos.py
+++ c_modelos.py
@@ -13,6 +13,7 @@ import joblib
 import a_funciones as fn
 from sklearn.neighbors import NearestNeighbors
 
+
 ###         CARGAR DATOS
 ### --------------------------------------------------------------------------------
 
@@ -31,112 +32,6 @@ df_final2 = pd.read_sql('''SELECT user_id, movie_id, title,
                         datetime(timestamp, 'unixepoch') AS rating_date
                         FROM final_ratings;''', conn)
 
-
-####################################################################################
-######################## 1. SISTEMAS BASADOS EN POPULARIDAD ########################
-####################################################################################
-
-
-###         TOP 10 (PELÍCULAS MEJORES CALIFICADAS)
-### --------------------------------------------------------------------------------
-
-top_score = pd.read_sql("""SELECT title, 
-                ROUND(AVG(movie_rating), 2) AS score,
-                COUNT(*) as views
-                FROM final_ratings
-                GROUP by title
-                HAVING views >= 20 
-                ORDER by score desc
-                limit 10
-                """, conn)
-
-# Extraer el título de la película #1
-top_movie = top_score.iloc[0]['title']
-
-# Eliminar los últimos 6 caracteres
-top_movie = top_movie[:-6]
-
-poster = fn.fetch_movie_poster(top_movie)
-
-
-###         TOP 10 (PELÍCULAS MÁS VISTAS)
-### --------------------------------------------------------------------------------
-
-top_score = pd.read_sql("""SELECT title, 
-                ROUND(AVG(movie_rating), 2) AS score,
-                COUNT(*) as views
-                FROM final_ratings
-                GROUP by title
-                HAVING score >= 4 
-                ORDER BY views desc
-                LIMIT 10
-                """, conn)
-
-# Extraer el título de la película #1
-top_movie = top_score.iloc[0]['title']
-
-# Eliminar los últimos 6 caracteres
-top_movie = top_movie[:-6]
-
-fn.fetch_movie_poster(top_movie)
-top_score
-
-###         TOP 10 (PELÍCULAS MÁS VISTAS EN EL ÚLTIMO AÑO)
-### --------------------------------------------------------------------------------
-
-
-
-top_score = pd.read_sql('''
-            SELECT title, 
-            ROUND(AVG(movie_rating), 2) AS score,
-            COUNT(movie_id) AS views
-            FROM final_ratings
-            WHERE strftime('%Y', datetime(timestamp, 'unixepoch')) = '2018'
-            GROUP BY title
-            HAVING score >= 4
-            ORDER BY views DESC
-            LIMIT 10;
-''', conn)
-
-# Extraer el título de la película #1
-top_movie = top_score.iloc[0]['title']
-
-# Eliminar los últimos 6 caracteres
-top_movie = top_movie[:-6]
-
-fn.fetch_movie_poster(top_movie)
-top_score
-
-
-###         TOP 10 (PELÍCULAS MÁS VISTAS EN EL ÚLTIMO MES)
-### --------------------------------------------------------------------------------
-
-top_score = pd.read_sql('''
-            SELECT title, 
-            ROUND(AVG(movie_rating), 2) AS score,
-            COUNT(movie_id) AS views
-            FROM final_ratings
-            WHERE strftime('%Y-%M', datetime(timestamp, 'unixepoch')) = '2018-08'
-            GROUP BY title
-            HAVING score >= 4
-            ORDER BY views DESC
-            LIMIT 10;
-''', conn)
-
-# Extraer el título de la película #1
-top_movie = top_score.iloc[0]['title']
-
-# Eliminar los últimos 6 caracteres
-top_movie = top_movie[:-6]
-
-fn.fetch_movie_poster(top_movie)
-top_score
-
-
-############################################################################################
-####### 2.1 Sistema de recomendación basado en contenido un solo producto - Manual #########
-############################################################################################
-
 ## ----- SEPARAR EL AÑO EN UNA NUEVA VARIABLE
 
 df_final2['year'] = df_final2['title'].str.extract(r'\((\d{4})\)')  # Extrae el año
@@ -154,6 +49,142 @@ df_final3 = pd.concat([df_final2, genres_dummies], axis=1)
 
 # Eliminar las columnas de 'genres' y 'timestamp'
 df_final3.drop(['genres'], axis=1, inplace=True)
+
+
+####################################################################################
+######################## 1. SISTEMAS BASADOS EN POPULARIDAD ########################
+####################################################################################
+
+###         TOP 10 (PELÍCULAS MEJORES CALIFICADAS - GLOBAL)
+### --------------------------------------------------------------------------------
+
+top_a = pd.read_sql("""SELECT title, 
+                ROUND(AVG(movie_rating), 2) AS score,
+                COUNT(*) as views
+                FROM final_ratings
+                GROUP by title
+                HAVING views >= 20 
+                ORDER by score desc
+                limit 10
+                """, conn)
+
+
+###         TOP 10 (PELÍCULAS MÁS VISTAS - GLOBAL)
+### --------------------------------------------------------------------------------
+
+top_b = pd.read_sql("""SELECT title, 
+                ROUND(AVG(movie_rating), 2) AS score,
+                COUNT(*) as views
+                FROM final_ratings
+                GROUP by title
+                HAVING score >= 4 
+                ORDER BY views desc
+                LIMIT 10
+                """, conn)
+
+###         TOP 10 (PELÍCULAS MÁS VISTAS EN EL ÚLTIMO AÑO)
+### --------------------------------------------------------------------------------
+
+top_c = pd.read_sql('''  
+            SELECT title, 
+            COUNT(fr.movie_id) AS views,
+            (SELECT ROUND(AVG(movie_rating), 2) 
+             FROM final_ratings 
+             WHERE movie_id = fr.movie_id) AS score
+            FROM final_ratings fr
+            WHERE strftime('%Y', datetime(fr.timestamp, 'unixepoch')) = "2018"
+            GROUP BY title
+            HAVING score >= 4
+            ORDER BY views DESC
+            LIMIT 10''', conn)
+
+
+###         TOP 10 (PELÍCULAS MEJORES CALIFICADAS EN EL ÚLTIMO AÑO)
+### --------------------------------------------------------------------------------
+
+top_d = pd.read_sql('''  
+            SELECT title, 
+            COUNT(fr.movie_id) AS views,
+            (SELECT ROUND(AVG(movie_rating), 2) 
+             FROM final_ratings 
+             WHERE movie_id = fr.movie_id) AS score
+            FROM final_ratings fr
+            WHERE strftime('%Y', datetime(fr.timestamp, 'unixepoch')) = "2018"
+            GROUP BY title
+            HAVING views >= 10
+            ORDER BY score DESC
+            LIMIT 10''', conn)
+
+
+###         TOP 10 (PELÍCULAS MÁS VISTAS POR GÉNERO)
+### --------------------------------------------------------------------------------
+import ipywidgets as widgets
+from IPython.display import display
+from prettytable import PrettyTable
+
+
+# Obtener los géneros únicos
+genres = ['Action', 'Adventure', 'Animation', 'Children', 'Comedy', 'Drama', 
+          'Fantasy', 'Film-Noir', 'Horror', 'IMAX', 'Musical', 'Mystery', 
+          'Romance', 'Sci-Fi', 'Thriller', 'War', 'Western']
+
+# Crear un widget de lista desplegable
+genre_dropdown = widgets.Dropdown(
+    options=genres,
+    description='Select Genre:',
+    value='Action'  # Género por defecto
+)
+
+def show_top_movies(selected_genre):
+    # Filtrar las películas según el género seleccionado
+    genre_movies = df_final3[df_final3[selected_genre] == 1]
+
+    # Agrupar por título y calcular el promedio de las calificaciones y el conteo de vistas
+    top_movies = genre_movies.groupby('title').agg(
+        score=('movie_rating', 'mean'),  # Promedio de ratings
+        views=('movie_rating', 'count')   # Contar el número de calificaciones
+    ).reset_index()
+
+    # Renombrar la columna del promedio
+    top_movies['score'] = top_movies['score'].round(2)  # Redondear a 2 decimales
+
+    # Filtrar por score mayor o igual a 4
+    top_movies = top_movies[top_movies['score'] >= 4]
+
+    # Ordenar por la puntuación en orden descendente y obtener las 10 mejores
+    top_movies = top_movies.sort_values(by='views', ascending=False).head(10)
+
+    # Mostrar el resultado
+    # Extraer el título de la película #1
+    top_movie = top_movies.iloc[0]['title']
+
+    fn.fetch_movie_poster(top_movie)
+
+    print("La película número 1 es: ", top_movie)
+    print("")
+    print("Aquí un listado con las películas que te podrían interesar: ")
+    print("")
+    
+    # Usar PrettyTable para alinear
+    table = PrettyTable()
+    table.field_names = ["Title", "Score", "Views"]
+    for index, row in top_movies.iterrows():
+        table.add_row([row['title'], row['score'], row['views']])
+
+    # Alinear a la derecha
+    table.align = "l"
+    print(table)
+
+    print("")
+    
+
+# Conectar la función al evento de cambio de selección
+widgets.interactive(show_top_movies, selected_genre=genre_dropdown)
+
+
+############################################################################################
+####### 2.1 Sistema de recomendación basado en contenido un solo producto - Manual #########
+############################################################################################
 
 ## ----- SISTEMA DE RECOMENDACIÓN BASADO EN CONTENIDO KNN
 
@@ -200,3 +231,10 @@ print(interact(MovieRecommender, movie_name=movie_titles))
 
 
 
+# Extraer el título de la película #1
+top_movie = top_a.iloc[0]['title']
+
+# Eliminar los últimos 6 caracteres
+top_movie = top_movie[:-6]
+
+poster = fn.fetch_movie_poster(top_movie)
